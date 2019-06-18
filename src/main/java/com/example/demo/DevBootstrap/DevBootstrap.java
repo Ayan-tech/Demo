b180a1759c372319bb1ef312f2993e012fb2522e
Ayan-tech <ayanbasu23@gmail.com>
2019-06-19 02:36:57 +0530

    publisher entity

--- src/main/java/com/example/demo/DevBootstrap/DevBootstrap.java
+++ src/main/java/com/example/demo/DevBootstrap/DevBootstrap.java
@@ -2,6 +2,7 @@ package com.example.demo.DevBootstrap;
 
 import com.example.demo.model.Author;
 import com.example.demo.model.Book;
+import com.example.demo.model.Publisher;
 import com.example.demo.repositories.AuthorRepository;
 import com.example.demo.repositories.BookRepository;
 import org.springframework.context.ApplicationListener;
@@ -28,7 +29,8 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Eric
         Author eric = new Author("Eric","Evans");
-        Book ddd = new Book("Domain Driven Design","1234","Harper Collins");
+        Publisher mcGrawHill = new Publisher("MacGrawHill","London");
+        Book ddd = new Book("Domain Driven Design","1234",mcGrawHill);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -37,7 +39,8 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Rod
         Author rod = new Author("Rod","Johnson");
-        Book noEJB = new Book("J2EE Development without EJB","2344","MORX");
+        Publisher arrihant = new Publisher("Arrihanth","America");
+        Book noEJB = new Book("J2EE Development without EJB","2344",arrihant);
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
